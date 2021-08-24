9a2c0b32052b1495e0af28e26d95c8e54c8fb1e9
Sowmya <sowmyaramesh4101@gmail.com>
2021-08-24 17:49:34 +0530

    added code for frontend

---
+++ backend/app.py
@@ -0,0 +1,11 @@
+from flask import Flask,request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/getTest")
+def getData():
+    data ={"id":2,"name":"Anna"}
+    return data
+
