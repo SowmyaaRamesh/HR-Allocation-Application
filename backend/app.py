6d2aafd4ed2f215221c72581278e52d27f731e1a
Karthik <Karthikraja2k1@gmail.com>
2021-08-25 11:55:04 +0530

    Post test done

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,15 @@
-from flask import Flask,request
+from flask import Flask,request,render_template
 from flask_cors import CORS
+import os
+import json
 
-app = Flask(__name__)
+template_dir = os.path.dirname(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+template_dir = os.path.join(template_dir, 'HR-Allocation-Application')
+# template_dir = os.path.join(template_dir, 'frontend')
+
+print(template_dir)
+
+app = Flask(__name__,template_folder=template_dir)
 CORS(app)
 
 @app.route("/getTest")
@@ -9,5 +17,11 @@ def getData():
     data ={"id":2,"name":"Anna"}
     return data
 
-@app.route("\postTest"):
+@app.route("/postTest",methods = ['GET', 'POST'])
 def postData():
+    if request.method == 'POST':
+        print(request.data)
+        recvd=request.data.decode('utf-8')#dict(request.data)
+        jsonrecvd=json.loads(recvd)
+        jsonrecvd.update({"recvd":1})
+        return jsonrecvd
