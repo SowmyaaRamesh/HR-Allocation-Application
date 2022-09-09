c87c27903126de515c30b367566ff6591b8596a7
karthikraja <karthikraja2k1@gmail.com>
2022-09-10 03:47:59 +0530

    pie chart broungth in under One component FormInput

--- Web Application - Front End/src/components/FormInput.js
+++ Web Application - Front End/src/components/FormInput.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import styles from "../styles/FormInput.module.css";
 import Button from "@mui/material/Button";
 import axios from "axios";
+import DownloadButton from "../components/DownloadButton";
+import OverviewChart from "../components/OverviewChart";
 // import saveAs from "file-saver";
 
 const FormInput = (props) => {
@@ -82,141 +84,148 @@ const FormInput = (props) => {
     }
   };
   return (
-    <div>
-      <form className={styles.input__container} onSubmit={submitHandler}>
-        <h2> Optimal Team Allocation</h2>
-        <p>{msg}</p>
-        <label htmlFor="numberOfTeams">Number of teams</label>
+    <div className={styles.flex__container}>
+      <div>
+        <form className={styles.input__container} onSubmit={submitHandler}>
+          <h2> Optimal Team Allocation</h2>
+          <p>{msg}</p>
+          <label htmlFor="numberOfTeams">Number of teams</label>
 
-        <div className={styles.row__group}>
-          <input
-            type="text"
-            disabled
-            name="teamsValue"
-            id="teamsValue"
-            value={teamNumInput}
-          />
-          <input
-            type="button"
-            name="numberOfTeams"
-            id="numberOfTeams"
-            value={"+"}
-            onClick={handleTeamNumInput}
-          />
-          <input
-            type="button"
-            name="numberOfTeams"
-            id="numberOfTeams"
-            value={"-"}
-            onClick={handleTeamNumInputSub}
-          />
-        </div>
-        <label htmlFor="typeOfEngineer">
-          Choose requirements for the project
-        </label>
-
-        {teamRequirements.map((team, index) => (
-          <div key={index}>
-            <h5>Team {index + 1} details:</h5>
-            <select
-              name="type1"
-              id="type1"
-              value={team.type1}
-              onChange={(e) => handleTeamRequirementsChange(index, e)}
-            >
-              <option value="default">Type of engineer </option>
-              <option value="sde">SDE</option>
-              <option value="nfit">NFIT</option>
-              <option value="sdn">SDN</option>
-              <option value="ne">NE</option>
-            </select>
-            <select
-              name="lvl_type1"
-              id="lvl_type1"
-              value={team.lvl_type1}
-              onChange={(e) => handleTeamRequirementsChange(index, e)}
-            >
-              <option value="default">Max experience level</option>
-              <option value="1">Level 1</option>
-              <option value="2">Level 2</option>
-              <option value="3">Level 3</option>
-            </select>
-            <select
-              name="type2"
-              id="type2"
-              value={team.type2}
-              onChange={(e) => handleTeamRequirementsChange(index, e)}
-            >
-              <option value="default">Type of engineer </option>
-              <option value="sde">SDE</option>
-              <option value="nfit">NFIT</option>
-              <option value="sdn">SDN</option>
-              <option value="ne">NE</option>
-            </select>
-            <select
-              name="lvl_type2"
-              id="lvl_type2"
-              value={team.lvl_type2}
-              onChange={(e) => handleTeamRequirementsChange(index, e)}
-            >
-              <option value="default">Max experience level</option>
-              <option value="1">Level 1</option>
-              <option value="2">Level 2</option>
-              <option value="3">Level 3</option>
-            </select>
-            <select
-              name="type3"
-              id="type3"
-              value={team.type3}
-              onChange={(e) => handleTeamRequirementsChange(index, e)}
-            >
-              <option value="default">Type of engineer </option>
-              <option value="sde">SDE</option>
-              <option value="nfit">NFIT</option>
-              <option value="sdn">SDN</option>
-              <option value="ne">NE</option>
-            </select>
-            <select
-              name="lvl_type3"
-              id="lvl_type3"
-              value={team.lvl_type3}
-              onChange={(e) => handleTeamRequirementsChange(index, e)}
-            >
-              <option value="default">Max experience level</option>
-              <option value="1">Level 1</option>
-              <option value="2">Level 2</option>
-              <option value="3">Level 3</option>
-            </select>
-            <select
-              name="type4"
-              id="type4"
-              value={team.type4}
-              onChange={(e) => handleTeamRequirementsChange(index, e)}
-            >
-              <option value="default">Type of engineer </option>
-              <option value="sde">SDE</option>
-              <option value="nfit">NFIT</option>
-              <option value="sdn">SDN</option>
-              <option value="ne">NE</option>
-            </select>
-            <select
-              name="lvl_type4"
-              id="lvl_type4"
-              value={team.lvl_type4}
-              onChange={(e) => handleTeamRequirementsChange(index, e)}
-            >
-              <option value="default">Max experience level</option>
-              <option value="1">Level 1</option>
-              <option value="2">Level 2</option>
-              <option value="3">Level 3</option>
-            </select>
+          <div className={styles.row__group}>
+            <input
+              type="text"
+              disabled
+              name="teamsValue"
+              id="teamsValue"
+              value={teamNumInput}
+            />
+            <input
+              type="button"
+              name="numberOfTeams"
+              id="numberOfTeams"
+              value={"+"}
+              onClick={handleTeamNumInput}
+            />
+            <input
+              type="button"
+              name="numberOfTeams"
+              id="numberOfTeams"
+              value={"-"}
+              onClick={handleTeamNumInputSub}
+            />
           </div>
-        ))}
+          <label htmlFor="typeOfEngineer">
+            Choose requirements for the project
+          </label>
 
-        <Button type="submit" className={styles.btn} variant="contained">
-          Generate Team
-        </Button>
-      </form>
+          {teamRequirements.map((team, index) => (
+            <div key={index}>
+              <h5>Team {index + 1} details:</h5>
+              <select
+                name="type1"
+                id="type1"
+                value={team.type1}
+                onChange={(e) => handleTeamRequirementsChange(index, e)}
+              >
+                <option value="default">Type of engineer </option>
+                <option value="sde">SDE</option>
+                <option value="nfit">NFIT</option>
+                <option value="sdn">SDN</option>
+                <option value="ne">NE</option>
+              </select>
+              <select
+                name="lvl_type1"
+                id="lvl_type1"
+                value={team.lvl_type1}
+                onChange={(e) => handleTeamRequirementsChange(index, e)}
+              >
+                <option value="default">Max experience level</option>
+                <option value="1">Level 1</option>
+                <option value="2">Level 2</option>
+                <option value="3">Level 3</option>
+              </select>
+              <select
+                name="type2"
+                id="type2"
+                value={team.type2}
+                onChange={(e) => handleTeamRequirementsChange(index, e)}
+              >
+                <option value="default">Type of engineer </option>
+                <option value="sde">SDE</option>
+                <option value="nfit">NFIT</option>
+                <option value="sdn">SDN</option>
+                <option value="ne">NE</option>
+              </select>
+              <select
+                name="lvl_type2"
+                id="lvl_type2"
+                value={team.lvl_type2}
+                onChange={(e) => handleTeamRequirementsChange(index, e)}
+              >
+                <option value="default">Max experience level</option>
+                <option value="1">Level 1</option>
+                <option value="2">Level 2</option>
+                <option value="3">Level 3</option>
+              </select>
+              <select
+                name="type3"
+                id="type3"
+                value={team.type3}
+                onChange={(e) => handleTeamRequirementsChange(index, e)}
+              >
+                <option value="default">Type of engineer </option>
+                <option value="sde">SDE</option>
+                <option value="nfit">NFIT</option>
+                <option value="sdn">SDN</option>
+                <option value="ne">NE</option>
+              </select>
+              <select
+                name="lvl_type3"
+                id="lvl_type3"
+                value={team.lvl_type3}
+                onChange={(e) => handleTeamRequirementsChange(index, e)}
+              >
+                <option value="default">Max experience level</option>
+                <option value="1">Level 1</option>
+                <option value="2">Level 2</option>
+                <option value="3">Level 3</option>
+              </select>
+              <select
+                name="type4"
+                id="type4"
+                value={team.type4}
+                onChange={(e) => handleTeamRequirementsChange(index, e)}
+              >
+                <option value="default">Type of engineer </option>
+                <option value="sde">SDE</option>
+                <option value="nfit">NFIT</option>
+                <option value="sdn">SDN</option>
+                <option value="ne">NE</option>
+              </select>
+              <select
+                name="lvl_type4"
+                id="lvl_type4"
+                value={team.lvl_type4}
+                onChange={(e) => handleTeamRequirementsChange(index, e)}
+              >
+                <option value="default">Max experience level</option>
+                <option value="1">Level 1</option>
+                <option value="2">Level 2</option>
+                <option value="3">Level 3</option>
+              </select>
+            </div>
+          ))}
+
+          <Button type="submit" className={styles.btn} variant="contained">
+            Generate Team
+          </Button>
+        </form>
+      </div>
+      <div className={styles.chart__right}>
+        <h2>Overview of Team Distribution</h2>
+        <OverviewChart />
+        <DownloadButton />
+      </div>
     </div>
   );
 };
