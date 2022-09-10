021bd222699c9aa1b10b13096616acf2b3571792
anirudh <anirudh07.anand@gmail.com>
2022-09-10 09:52:01 +0530

    latest version

--- Web Application - Front End/src/components/OverviewChart.js
+++ Web Application - Front End/src/components/OverviewChart.js
@@ -9,7 +9,7 @@ export const data = {
   datasets: [
     {
       label: "# of people",
-      data: [12, 6, 3, 5],
+      data: [5, 8, 0, 0],
       backgroundColor: [
         "rgba(255, 99, 132, 0.2)",
         "rgba(54, 162, 235, 0.2)",
