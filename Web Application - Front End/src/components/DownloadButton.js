e9e9fe3f01995bc4b4f005846f322437ef5eac9d
anirudh <anirudh07.anand@gmail.com>
2022-09-27 12:41:05 +0530

    rename file path

--- Web Application - Front End/src/components/DownloadButton.js
+++ Web Application - Front End/src/components/DownloadButton.js
@@ -2,21 +2,34 @@ import React, { useState } from "react";
 import Button from "@mui/material/Button";
 import { Link } from "react-router-dom";
 import styles from "../styles/Home.module.css";
+import axios from 'axios';
 
 const DownloadButton = (props) => {
   const [isDisabled, setIsDisabled] = useState(true);
   console.log(isDisabled);
+ const handler =()=>{axios({
+  url: 'http://localhost:5000/download', //your url
+  method: 'GET',
+  responseType: 'blob', // important
+}).then((response) => {
+  // create file link in browser's memory
+  const href = URL.createObjectURL(response.data);
 
+  // create "a" HTLM element with href to file & click
+  const link = document.createElement('a');
+  link.href = href;
+  link.setAttribute('download', 'file.xlsx'); //or any other extension
+  document.body.appendChild(link);
+  link.click();
+
+  // clean up "a" element & remove ObjectURL
+  document.body.removeChild(link);
+  URL.revokeObjectURL(href);
+});}
   return (
-    <Button variant="contained" className={styles.btn} disabled={false}>
-      <Link
-        className={styles.download__link}
-        to="/assets/pandas_multiple.xlsx"
-        target="_blank"
-        download
-      >
+    <Button variant="contained" className={styles.btn} disabled={false} onClick={handler}>
         Download Detailed Structure
-      </Link>
+ 
     </Button>
   );
 };
